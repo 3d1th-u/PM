c1879ae3cb9cda84ff452d806469d26d03cde8cd
3d1th-u <uribeordonezedith@gmail.com>
2026-06-10 01:52:35 -0600

    PRACTICA6. Parte 1
    identificamos e hicimos uso de los componentes simples y compuestos, también la estructura de un proyecto y sus carpetas

--- practica5/miapp2/App.js
+++ practica5/miapp2/App.js
@@ -1,15 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+/*zona1: importaciones de componentes y archivos */
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image} from 'react-native';
+import {Saludo} from './components/Saludo';
+import {Salu2} from './components/Salu2';
+
+
+/*zona2: main - hogar de los componetes */
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Image source={require('./assets/wave.png')}/>
+
+      <Text>Holaaaa mundo RN :p</Text>
+
+      <Text>=================================componente simple=========================================</Text>
+      <Saludo></Saludo>
+      <Text>=================================componente compuesto=========================================</Text>
+      <Salu2></Salu2>
+
       <StatusBar style="auto" />
+
     </View>
   );
 }
 
+/*zona3: estilos y posicionamiento */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
---
+++ practica5/miapp2/components/Salu2.js
@@ -0,0 +1,12 @@
+import { Button, Text, Image, View} from "react-native";
+
+export const Salu2= () =>{
+    return(
+        <View>
+
+        <Text>Soy un componete propiooo</Text>
+        <Image source={require('../assets/wave.png')}/>
+        <Button title="Holaaa"></Button>
+        </View>
+    )
+}
