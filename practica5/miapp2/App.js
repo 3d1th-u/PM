2b824f2d39b9e16eeabf8309b31c65713d05629c
3d1th-u <uribeordonezedith@gmail.com>
2026-06-12 23:33:27 -0600

    Practica 7
    hicimos uso de la desestructuracion y algo parecido a un menu hamburguesa con un boton

--- practica5/miapp2/App.js
+++ practica5/miapp2/App.js
@@ -4,26 +4,22 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image} from 'react-native';
 import {Saludo} from './components/Saludo';
 import {Salu2} from './components/Salu2';
-import { Perfil1 } from './components/Perfil';
-
+import { Perfil2 } from './components/Perfil';
 
 /*zona2: main - hogar de los componetes */
 export default function App() {
   return (
     <View style={styles.container}>
-
-      <Image source={require('./assets/wave.png')}/>
-
-      <Text>Holaaaa mundo RN :p</Text>
-
-      <Text>=================================componente simple=========================================</Text>
-      <Saludo></Saludo>
-      <Text>=================================componente compuesto=========================================</Text>
-      <Salu2></Salu2>
-      <Text>=================================Perfil=========================================</Text>
-      <Perfil1></Perfil1>
-
-      <StatusBar style="auto" />
+      
+      <Perfil2
+      nombre="Edith Uribe" 
+      carrera="Sitemas" 
+      materia="programacion movil" 
+      cuatri="noveno"
+      >
+      </Perfil2>
+      <Text>----------------------------------------------------------------</Text>
+      <Text>----------------------------------------------------------------</Text>   
 
     </View>
   );
--- practica5/miapp2/components/Perfil.js
+++ practica5/miapp2/components/Perfil.js
@@ -1,13 +1,39 @@
-import { Text, View} from "react-native";
+/*perfil usando desestructuracion */
+import { Text, View, Button} from "react-native";
+import React,{useState} from "react";
 
-export const Perfil1= () =>{
+   export const Perfil2= ({nombre,carrera,materia,cuatri}) =>{
+    
+    const [mostrar, setmostrar] = useState(false);
+
     return(
         <View>
 
-        <Text>Edith Uribe Ordoñez</Text>
-        <Text>Ingeniería en Sistemas Computacionales</Text>
-        <Text>Programación Móvil</Text>
-        <Text>Noveno cuatrimestre</Text>
+        <Text>{nombre}</Text>
+
+        {mostrar &&
+        <>
+        <Text>{carrera}</Text>
+        <Text>{materia}</Text>
+        <Text>{cuatri}</Text>
+        </>
+         }
+        <Button 
+        title="Mostrar perfil" 
+        onPress={()=> setmostrar(!mostrar)}></Button>
         </View>
     )
-}
+}   
+
+
+/* */
+/*    export const Perfil2= (props) =>{
+    return(
+        <View>
+
+        <Text>{props.nombre}</Text>
+        <Text>{props.carrera}</Text>
+        <Text>{props.materia}</Text>
+        <Text>{props.cuatri}</Text>
+        </View>
+    ) */
